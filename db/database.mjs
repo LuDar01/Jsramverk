36d2918e059b067e1b861f39d0e0849f0c8e07eb
Lutfia <luda22@student.bth.se>
2026-03-08 03:02:57 +0100

    add mongo atlas

--- db/database.mjs
+++ db/database.mjs
@@ -1,15 +1,15 @@
 import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 const collectionName = "documents";
 
 const database = {
     getDb: async function getDb() {
-        // This is your local connection string
-        let dsn = "mongodb://localhost:27017/jsramverk";
 
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.lrmr5th.mongodb.net/folinodocs?retryWrites=true&w=majority`;
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
-        }
+        } 
 
         const client = await MongoClient.connect(dsn);
         const db = await client.db();
@@ -23,6 +23,32 @@ const database = {
 };
 
 export default database;
+
+// let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.hkfbt.mongodb.net/folinodocs?retryWrites=true&w=majority`;
+
+// const collectionName = "documents";
+
+// const database = {
+//     getDb: async function getDb() {
+//         // This is your local connection string
+//         let dsn = "mongodb://localhost:27017/jsramverk";
+
+//         if (process.env.NODE_ENV === 'test') {
+//             dsn = "mongodb://localhost:27017/test";
+//         }
+
+//         const client = await MongoClient.connect(dsn);
+//         const db = await client.db();
+//         const collection = await db.collection(collectionName);
+
+//         return {
+//             collection: collection,
+//             client: client,
+//         };
+//     }
+// };
+
+// export default database;
 
 // import sqlite3 from 'sqlite3';
 // import { open } from 'sqlite';
